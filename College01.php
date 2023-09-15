5881d7daedd8221723ab5b41f7558502bc22d853
JariKN01 <jariknoop2@gmail.com>
2023-09-15 12:48:25 +0200

    New updates in bestand

--- College01.php
+++ College01.php
@@ -1,5 +1,4 @@
 <?php
-
 require 'functions/function.php';
 
 echo "<h3>Variable maken</h3>";
@@ -8,22 +7,27 @@ $name = 'Jari';
 $age = 21;
 $isMarried = false;
 $dob = date('2,11,2001');
+$postcode = "1336KT";
+$city = "Almere";
+$speaksDutch = true;
+$email = "jari@gmail.com";
+$number = "06" . "12345678";
+$skills = "webdevelopment";
 
 // datum format veranderen naar dag-maand-jaar
-$newDob = date('d-m-Y', strtotime(str_ireplace(',', '-', $dob)));
+$newDob = date('d-m-Y', strtotime(str_ireplace(',','-', $dob)));
 $dobVeranderd = date('d, F Y', strtotime($newDob));
 
-echo "Naam is:  " . $name . "<br>";
-echo "Leeftijd is: " . $age . "<br>";
-echo "Getrouwd: " . $isMarried;
-
+echo "Naam is:  ".$name."<br>";
+echo "Leeftijd is: ".$age."<br>";
+echo "Mailadres is; ".$email."<br>";
+echo "Getrouwd: ".$isMarried;
 // if statement om te checken of persoon getrouwd is
-if ($isMarried == false) {
-    echo "single" . "<br>";
+if ($isMarried == false){
+    echo "single". "<br>";
 } else {
-    echo "Getrouwd" . "<br>";
+    echo "Getrouwd"."<br>";
 }
-
-echo "Geboren is op: " . $dobVeranderd . "<br>" . "<br>";
-
+echo "Geboren is op: ".$dobVeranderd."<br>";
+echo "Aders is: " . $postcode . " " . $city;
 
