afebe18b72ca117732ea41952236d2ed53c7bc19
JariKN01 <jariknoop2@gmail.com>
2023-09-15 13:17:43 +0200

    Added function colors en greeting with HTML elements

--- College01.php
+++ College01.php
@@ -1,7 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1 style="color: <?php echo backgroundColor(); ?>">Variabelen maken en kleur aanpassen</h1><hr>
+<h2>Goeie <?php echo groet(); ?></h2><hr>
 <?php
+
 require 'functions/function.php';
 
-echo "<h3>Variable maken</h3>";
 // Variable maken
 $name = 'Jari';
 $age = 21;
@@ -23,11 +30,50 @@ echo "Leeftijd is: ".$age."<br>";
 echo "Mailadres is; ".$email."<br>";
 echo "Getrouwd: ".$isMarried;
 // if statement om te checken of persoon getrouwd is
-if ($isMarried == false){
+if ($isMarried = false){
     echo "single". "<br>";
 } else {
     echo "Getrouwd"."<br>";
 }
 echo "Geboren is op: ".$dobVeranderd."<br>";
-echo "Aders is: " . $postcode . " " . $city;
+echo "Aders is: " . $postcode . " " . $city."<br>";
+echo "Vaardigheden: ".$skills."<br>";
+// if statement om te checken of iemand nederlands spreekt
+if ($speaksDutch = true){
+    echo "Spreekt Nederlands" . "<br>";
+} else {
+    echo "Spreekt geen Nederlands" . "<br>";
+}
 
+date('H:i');
+
+function backgroundColor(){
+    $backgroundColor = '';
+    if (strtotime(date('H:i')) >= strtotime('06:00') && strtotime(date('H:i')) <= strtotime('12:00')){
+        $backgroundColor = '#B0B3D6';
+    } elseif (strtotime(date('H:i')) >= strtotime('12:00') && strtotime(date('H:i')) <= strtotime('18:00')){
+        $backgroundColor = '#787CB5';
+    } elseif (strtotime(date('H:1')) >= strtotime('18:00') && strtotime(date('H:i')) <= strtotime('24:00')){
+        $backgroundColor = '#b14fdb';
+    } else {
+        $backgroundColor = '#303469';
+    }
+    return $backgroundColor;
+}
+
+function groet(){
+    $groet = '';
+    if (strtotime(date('H:i')) >= strtotime('06:00') && strtotime(date('H:i')) <= strtotime('12:00')){
+        $groet = 'Morgen';
+    } elseif (strtotime(date('H:i')) >= strtotime('12:00') && strtotime(date('H:i')) <= strtotime('18:00')) {
+        $groet = 'Middag';
+    } elseif (strtotime(date('H:i')) >= strtotime('18:00') && strtotime(date('H:i')) <= strtotime('24:00')) {
+        $groet = 'Avond';
+    } elseif (strtotime(date('H:i')) >+ strtotime('24:00') && strtotime(date('H:i')) <= strtotime('06:00')) {
+        $groet = 'Nacht';
+    }
+    return $groet;
+}
+?>
+</body>
+</html>
