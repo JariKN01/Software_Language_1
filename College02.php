9c4b22ffe6020be79b84c0927baf6d4568d0b31c
JariKN01 <jariknoop2@gmail.com>
2023-09-15 13:21:55 +0200

    New small updates with for loops

--- College02.php
+++ College02.php
@@ -1,26 +1,32 @@
 <?php
-
 require 'functions/function.php';
 
 echo "<h1>Array maken</h1>";
 // Array maken
-$jari = ['Jari', 'Knoop', 'jari.knoop@windesheim.nl', 'age' => 21];
+$jari = ['Jari', 'Knoop', 'jari.knoop@windesheim.nl','age' => 21];
 
-echo "Naam is: " . $jari[0] . "<br>";
-echo "Achternaam is: " . $jari[1] . "<br>";
-echo "Mail is: " . $jari[2] . "<br>";
-echo "Is " . $jari['age'] . "jaar oud";
+echo "Naam is: " .$jari[0]."<br>";
+echo "Achternaam is: " . $jari[1]."<br>";
+echo "Mail is: " . $jari[2]. "<br>";
+echo "Is ". $jari['age']."jaar oud";
 
 echo "<h1>Foreach loop</h1>";
 // foreach loop
 
-$klas = ['Persoon 1', 'Persoon 2', 'Persoon 3', 'Persoon 4'];
 
-foreach ($klas as $leerling) {
+$klas = ['Persoon 1','Persoon 2','Persoon 3','Persoon 4'];
+
+foreach ($klas as $leerling){
     echo "Naam van leerling: " . $leerling . "<br>";
 }
 
 echo "<h1>For loop Array</h1>";
+
+$key = array_keys($jari);
+
+for ($i = 0; $i <count($klas); $i++){
+    echo "<li>" . $klas[$i] . "<br>";
+}
 
 // Multidimensional array maken
 $RJDA = [
@@ -34,13 +40,13 @@ sort($RJDA);
 
 echo "<h1>Foreach loop Multi Array</h1>";
 
-foreach ($RJDA as $key => $value) {
+//Het lukt mij niet om $key de titel van de array achter 'Deelnemer: ' te zetten als bijvoorbeeld 'Student 1'
+foreach ($RJDA as $key => $value){
     echo "<b>Deelnemer: " . $key . "</b><br><hr>";
-
     echo "<ul><li>" . $value['firstname'] . " ";
     echo $value['lastname'] . "<br>";
     echo "<li>" . "Leeftijd: " . $value['leeftijd'] . "<br>";
-    echo "<li>" . "Mailadres: " . $value['mail'] . "</ul>" . "<hr>";
+    echo "<li>" . "Mailadres: " . $value['mail'] . "</ul>" ."<hr>";
 }
 
 echo "<h1>For loop Multi Array</h1>";
